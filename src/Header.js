604c07d2961b604a3775409b758b03caef52c722
sarah <skg@neofilm.us>
2023-06-02 17:57:44 -0400

    Pass props, map over array, render note component

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Header = (props) => (
   <header className="app-header">
-    {console.log(props)} 
     <h1 className="app-header__title">Super Sticky Notes</h1>
     <aside className="app-header__controls">
       <button className="add-new">+ New Note</button>
